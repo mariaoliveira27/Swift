838fc29e352aea1c73fa8dadb1c5aca167746bf1
Maria Clara <mariaclariagomes@gmail.com>
2024-12-23 01:08:18 -0300

    Add

---
+++ CodigosSwift/CaixaEletronico.swift
@@ -0,0 +1,18 @@
+/*Vamos treinar? Faça um caixa eletrônico!  Vamos fazer um depósito, 
+crie um switch que imprima qual nota foi inserida, sua variável pode ser uma String ou um Int.*/
+
+var nota = 100
+
+switch nota {
+    case 200 :
+        print("Nota  de 200 inserida")
+    case 100:
+        print("Noat de 100 inserida")
+    case 50:
+        print("Nota de 50 inserida")
+    case 20:
+        print("Nota de 20 inserida")
+   default:
+        print("Nota inválida") 
+}
+
