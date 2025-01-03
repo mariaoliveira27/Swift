ff354d9d4dc3d08ef5328e0cd51e89b9999b7523
Maria Clara <mariaclariagomes@gmail.com>
2025-01-03 00:56:09 -0300

    Soma

---
+++ CodigosSwift/Soma.swift
@@ -0,0 +1,8 @@
+//Soma com entrada de dados
+if let inputA = readLine(), let inputB = readLine(){
+    if let A = Int(inputA), let B = Int(inputB){
+        let SOMA = A + B
+
+        print("SOMA = \(SOMA)")
+    }
+}
